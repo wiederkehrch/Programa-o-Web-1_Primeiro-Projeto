d01ae47e9c8892280cf2a5a35d38a5927a5b25ee
Sandra Wiederkehr <69729146+wiederkehrch@users.noreply.github.com>
2021-08-25 15:44:30 -0300

    Commit Cadastro Aluno

--- src/main/java/br/com/senac/controller/AlunoController.java
+++ src/main/java/br/com/senac/controller/AlunoController.java
@@ -3,9 +3,11 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.domain.Aluno;
 import br.com.senac.service.AlunoService;
 
 
@@ -29,5 +31,27 @@ public class AlunoController {
 		mv.addObject("alunos", alunoService.buscarTodosAlunos());
 		return mv;
 	}
+	
+	
+	//criar uma rota para chamar tela de cadastro
+	@GetMapping ("/cadastrarAluno")
+	public ModelAndView cadastrarAluno() {
+		
+		ModelAndView mv = new ModelAndView("aluno/cadastraAluno");
+		mv.addObject("aluno", new Aluno());
+		return mv;
+	}
+	
+	
+	//criar uma rota para incluir aluno
+	@PostMapping ("/salvarAluno")
+	public ModelAndView salvarAluno(Aluno aluno) {
+			
+		alunoService.salvar(aluno);
+		return listaTodosAlunos();
+		}
+	
+	
+	
 
 }
