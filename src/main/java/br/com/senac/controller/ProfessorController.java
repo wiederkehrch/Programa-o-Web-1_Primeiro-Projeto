e8487e9c5b1bd7f4fa9067c86de41f6bae3c4539
Sandra Wiederkehr <69729146+wiederkehrch@users.noreply.github.com>
2021-08-25 16:07:58 -0300

    Commit Cadastro Professor

--- src/main/java/br/com/senac/controller/ProfessorController.java
+++ src/main/java/br/com/senac/controller/ProfessorController.java
@@ -3,9 +3,11 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.domain.Professor;
 import br.com.senac.service.ProfessorService;
 
 @Controller                         
@@ -25,5 +27,24 @@ public class ProfessorController {
 		mv.addObject("professores", professorService.buscarTodosProfessores());
 		return mv;
 	}
+	
+	
+	//criar uma rota para chamar tela de cadastro
+		@GetMapping ("/cadastrarProfessor")
+		public ModelAndView cadastrarProfessor() {
+			
+			ModelAndView mv = new ModelAndView("professor/cadastraProfessor");
+			mv.addObject("professor", new Professor());
+			return mv;
+		}
+		
+		
+		//criar uma rota para incluir professor
+		@PostMapping ("/salvarProfessor")
+		public ModelAndView salvarProfessor(Professor professor) {
+				
+			professorService.salvar(professor);
+			return listaTodosProfessores();
+			}
 
 }
