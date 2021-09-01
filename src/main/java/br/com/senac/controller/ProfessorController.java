167078720987846bec81e207bde8c5e497325ad2
Sandra Wiederkehr <69729146+wiederkehrch@users.noreply.github.com>
2021-09-01 14:37:12 -0300

    Commit final CRUD Professor

--- src/main/java/br/com/senac/controller/ProfessorController.java
+++ src/main/java/br/com/senac/controller/ProfessorController.java
@@ -3,12 +3,14 @@ package br.com.senac.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.senac.domain.Professor;
 import br.com.senac.service.ProfessorService;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Controller                         
 @RequestMapping("professor") 
@@ -46,5 +48,34 @@ public class ProfessorController {
 			professorService.salvar(professor);
 			return listaTodosProfessores();
 			}
+		
+		
+		//criar uma rota para alterar professor
+		@GetMapping ("/alterarProfessor/{id}")
+		public ModelAndView alterarProfessor(@PathVariable("id") Integer idProfessor) throws ObjectNotFoundException {
+					
+			ModelAndView mv = new ModelAndView("professor/alterarProfessor");
+			mv.addObject("professor", professorService.buscaPorId(idProfessor));
+			return mv;
+		}
+		
+		
+		//criar uma rota para salvar professor alterado
+			@PostMapping ("/alterarProfessor")
+			public ModelAndView salvarAlteraProfessor(Professor professor) throws ObjectNotFoundException {
+					
+				professorService.salvarAlteracao(professor);
+				return listaTodosProfessores();
+			}
+		
+			
+			//criar uma rota para excluir professor
+			@GetMapping ("/excluirProfessor/{id}")
+			public ModelAndView excluirProfessor(@PathVariable("id") Integer idProfessor) throws ObjectNotFoundException {
+						
+				professorService.excluir(idProfessor);
+				return listaTodosProfessores();
+			}
+
 
 }
--- src/main/java/br/com/senac/service/ProfessorService.java
+++ src/main/java/br/com/senac/service/ProfessorService.java
@@ -1,12 +1,14 @@
 package br.com.senac.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.senac.domain.Professor;
 import br.com.senac.repository.ProfessorRepositorio;
+import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class ProfessorService {
@@ -24,4 +26,29 @@ public List<Professor> buscarTodosProfessores () {
 		
 		return repo.findAll();
 	}
+
+	
+public Professor buscaPorId (Integer id) throws ObjectNotFoundException {
+	
+	Optional<Professor> professor = repo.findById(id);
+	return professor.orElseThrow(() -> new ObjectNotFoundException ("Professor não encontrado" + id));
+}
+
+
+public Professor salvarAlteracao (Professor professorAlterado) throws ObjectNotFoundException {
+	
+	Professor professorAtual = buscaPorId(professorAlterado.getId());
+	professorAtual.setId(professorAlterado.getId());
+	professorAtual.setNome(professorAlterado.getNome());
+	return salvar(professorAtual);
+}
+
+
+public void excluir (Integer idProfessor) {
+	
+	repo.deleteById(idProfessor);
+}
+
+
+
 }
